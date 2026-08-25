a822054ac7899223b48332ca56513ff09e990302
Codefitness21 <kelly.m.hill2115@gmail.com>
2026-08-25 15:08:57 -0500

    tab logo added

--- src/components/SessionBtn.tsx
+++ src/components/SessionBtn.tsx
@@ -2,10 +2,10 @@ const SessionBtn = () => {
   const boxes = Array.from({ length: 5 }, (_, index) => index + 1);
   return (
     <div className="flex min-h-0 flex-1 flex-col">
-      {boxes.map((box) => (
+      {boxes.map((boxes) => (
         <div
           className="flex flex-1 items-center justify-center border-t border-dashed border-gray-400"
-          key={box}
+          key={boxes}
         >
           <button className="bg-white p-4 border-2 border-dashed border-gray-400 cursor-pointer">
             + add session
